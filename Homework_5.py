5ffee0c17004add369abcacd7ce3b497e75ce106
Verantina007 <105320531+Verantina007@users.noreply.github.com>
2022-10-18 10:18:08 +0300

    Update Homework_5.py

--- Homework_5.py
+++ Homework_5.py
@@ -1,7 +1,7 @@
 # #Homework_5
 import datetime as dt
 
-#define class for work with News
+# define class for work with News
 class News:
     def __init__(self, name_of_post='News', text='', city='', date=''):
         self.name_of_post = name_of_post
@@ -27,25 +27,25 @@ class PrivatAd:
         file.write(self.name_of_post+'\n'+self.text+'\nActual until: '+self.expiration_date+', '+str(self.number_of_days)+' days\n\n')
         file.close()
 
-# class WeatherForecast:
-#     def __init__(self, name_of_post='Weather forecast', degrees=''):
-#         self.name_of_post = name_of_post
-#         self.degrees = input('Enter the weather forecast for a day in format +/-20.2 C')
-# #        self.result = weather_condition(self, self.degrees)
-#
-#     def weather_condition(self):
-#         if startswith(degrees, '+'):
-#             self.result = 'hot weather'
-#         elif startswith(degrees, '-'):
-#             self.result = 'cold weather'
-#         elif degrees == '0 C':
-#             self.result = 'not hot or cold'
-#         return self.result
-#
-#     def publish(self):
-#         file = open('posts.txt', 'a')
-#         file.write(self.name_of_post+'\nThe weather forecast for today is: '+self.degrees+'\nWe arehaving '+weather_condition(self)+'\n\n')
-#         file.close()
+class WeatherForecast:
+    def __init__(self, name_of_post='Weather forecast', degrees=''):
+        self.name_of_post = name_of_post
+        self.degrees = input('Enter the weather forecast for a day in format +/-20.2 C')
+        self.result = self.weather_condition()
+
+    def weather_condition(self):
+        if self.degrees.startswith('+'):
+            self.result = 'hot weather'
+        elif self.degrees.startswith('-'):
+            self.result = 'cold weather'
+        elif self.degrees == '0 C':
+            self.result = 'not hot or cold'
+        return self.result
+
+    def publish(self):
+        file = open('posts.txt', 'a')
+        file.write(self.name_of_post+'\nThe weather forecast for today is: '+self.degrees+'\nWe are having '+self.result+'\n\n')
+        file.close()
 
 
 class ChoseYourPost:
@@ -57,8 +57,8 @@ class ChoseYourPost:
             News().publish()
         elif self.number_of_the_post == '2':
             PrivatAd().publish()
-        # elif self.number_of_the_post == '3':
-        #     WeatherForecast().publish()
+        elif self.number_of_the_post == '3':
+            WeatherForecast().publish()
         else:
             print ("You've entered the wrong number!")
 
