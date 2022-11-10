5ce386a582c9289b001a2d2783218c83b6ad2cc5
Verantina007 <105320531+Verantina007@users.noreply.github.com>
2022-11-10 22:57:21 +0300

    Update Homework_6.py

--- Homework_6.py
+++ Homework_6.py
@@ -9,32 +9,40 @@ class Publisher_file:  # class for choosing type of post
         file = open('posts_to_add.txt', 'r')
         lines = file.readlines()
         file.close()
-        for i in range(0, len(lines) - 1):
-            lines[i] = normalize_string(lines[i])
-            if lines[i] == '\n':
-                i = i + 1
-                pass
-            elif lines[i] == 'News.\n':
+        new_lines = []
+        for i in range(0, len(lines)):
+            if (lines[i] != '') & (lines[i] != '\n'):
+                new_lines.append(lines[i])
+        print(new_lines)
+
+        i = 0
+        while len(new_lines) >= 3:
+            i = 0
+            if 'news' in new_lines[i].lower():
                 self.post_name = 'News'
-                self.post_text = lines[i + 1][:-1]
-                self.post_param = lines[i + 2][:-1]
+                self.post_text = new_lines[i + 1][:-1]
+                self.post_param = new_lines[i + 2][:-1]
                 self.calculated_param = PostNews().calculate_date()
                 self.publish_post(self.calculated_param)
-                i = i + 3
-            elif lines[i] == 'Privatad.\n':
+                print(self.calculated_param, 'News added')
+                new_lines = new_lines[i + 3:]
+            elif 'privatad' in new_lines[i].lower():
                 self.post_name = 'PrivatAd'
-                self.post_text = lines[i + 1][:-1]
-                self.post_param = lines[i + 2][:-1]
+                self.post_text = new_lines[i + 1][:-1]
+                self.post_param = new_lines[i + 2][:-1]
                 self.calculated_param = PostPrivatAd().calculate_days(self.post_param)
                 self.publish_post(self.calculated_param)
-                i = i + 3
-            elif lines[i] == 'Weather forecast.\n':
+                print(self.calculated_param, 'Privatad added')
+                new_lines = new_lines[i + 3:]
+            elif 'weather forecast' in new_lines[i].lower():
                 self.post_name = 'Weather Forecast'
-                self.post_text = lines[i + 1][:-1]
-                self.post_param = lines[i + 2][:-1]
+                self.post_text = normalize_string(new_lines[i + 1])[:-2]
+                self.post_param = normalize_string(new_lines[i + 2])[:-2]
                 self.calculated_param = PostWeatherForecast().calculate_forecast(self.post_param)
                 self.publish_post(self.calculated_param)
-                i = i + 3
+                print(self.calculated_param, 'Weather forecast added')
+                new_lines = new_lines[i + 3:]
+
         os.remove('C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\posts_to_add.txt')
 
     def publish_post(self, calculated_param):  # publish for each type of posts
@@ -47,13 +55,20 @@ def define_format(input_post_format=''):
     if input_post_format == '1':
         Publisher().input_post()
     elif input_post_format == '2':
-        Publisher_file().input_post()
+        try:
+            open('C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\posts_to_add.txt', 'r')
+        except FileNotFoundError:
+            print('No such file or directory: "posts_to_add.txt"')
+            exit()
+        else:
+            Publisher_file().input_post()
     else:
         print(
             "You've entered the wrong number! Choose the correct post format (1 - Text, 2 - File with one post)")
 
 
-define_format()
+if __name__ == '__main__':
+    define_format()
 
 # # imported function from Homework_4.py
 # def normalize_string(str_for_normalization):  # function for normalization of the string
