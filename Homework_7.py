b45016cf17d97692ed29731c9ec66caa9b3b15db
Verantina007 <105320531+Verantina007@users.noreply.github.com>
2022-11-16 11:54:08 +0300

    Update Homework_7.py

--- Homework_7.py
+++ Homework_7.py
@@ -3,12 +3,15 @@
 import csv  # import module for work with csv
 import os  # import module for ability to delete files
 from Homework_6 import *  # import previous homeworks (5-6)
+# from parse_file_function import parse_file
 
 
 class CountWords:
-    def parse_file(self, list_with_words):
-        with open('posts.txt', 'r') as source_file:  # open file with posts
+    @staticmethod
+    def parse_file(self, path_file='C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\posts.txt'):
+        with open(path_file, 'r') as source_file:  # open file with posts
             reader = csv.reader(source_file)  # read each line of the file
+            list_with_words =  []
             for row in reader:
                 for element in row:
                     if element != []:
@@ -18,7 +21,8 @@ class CountWords:
                                 list_with_words.append(word.lower())
         return list_with_words
 
-    def count_number(self, dict_for_words, list_with_words):
+    def count_number(self, list_with_words):
+        dict_for_words = {}
         for word in list_with_words:
             if word in dict_for_words.keys():
                 dict_for_words[word] = dict_for_words[word] + 1
@@ -26,12 +30,11 @@ class CountWords:
                 dict_for_words[word] = 1
         return dict_for_words
 
-    def write_words_csv(self, list_with_words=[], dict_for_words={}):
-        if os.path.exists('C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\words.csv'):
-            os.remove(
-                'C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\words.csv')  # delete old file if exists
-        self.parse_file(list_with_words)
-        self.count_number(dict_for_words, list_with_words)
+    def write_words_csv(self, path_words='C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\words.csv'):
+        if os.path.exists(path_words):
+            os.remove(path_words)  # delete old file if exists
+        list_with_words = self.parse_file(self)
+        dict_for_words=self.count_number(list_with_words)
         with open('words.csv', 'w', newline='') as words_file:  # open file with posts
             writer = csv.writer(words_file, delimiter='-', quotechar='"',
                                 quoting=csv.QUOTE_ALL)  # read each line of the file
@@ -40,9 +43,11 @@ class CountWords:
 
 
 class CountLetters:
-    def parse_file(self, string_with_letters):
-        with open('posts.txt', 'r') as source_file:  # open file with posts
+    @staticmethod
+    def parse_file(self, path_file='C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\posts.txt'):
+        with open(path_file, 'r') as source_file:  # open file with posts
             reader = csv.reader(source_file)  # read each line of the file
+            string_with_letters=''
             for row in reader:
                 for element in row:
                     for char in element:
@@ -50,9 +55,9 @@ class CountLetters:
                             string_with_letters = string_with_letters + char
         return string_with_letters
 
-    def count_values(self, string_with_letters, dict_with_letters):
-        string_with_letters = self.parse_file(string_with_letters)
+    def count_values(self, string_with_letters):
         number_of_all_letters = len(string_with_letters)
+        dict_with_letters={}
         for letter in string_with_letters:
             if letter.lower() in dict_with_letters.keys():
                 number_of_letter = dict_with_letters[letter.lower()][0] + 1
@@ -70,13 +75,13 @@ class CountLetters:
                     number_of_uppercase = 0
                 persent_letter = (number_of_letter / number_of_all_letters) * 100
                 dict_with_letters[letter.lower()] = [number_of_letter, number_of_uppercase, persent_letter]
+        return dict_with_letters
 
-    def write_letters_csv(self, string_with_letters='', dict_with_letters={}):
-        if os.path.exists('C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\letters.csv'):
-            os.remove(
-                'C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\letters.csv')  # delete old file if exists
-        self.parse_file(string_with_letters)
-        self.count_values(string_with_letters, dict_with_letters)
+    def write_letters_csv(self, path_letters='C:\\Users\\Vera_Varsotskaya\\PycharmProjects\\pythonProject4\\venv\\letters.csv'):
+        if os.path.exists(path_letters):
+            os.remove(path_letters)  # delete old file if exists
+        string_with_letters = self.parse_file(self)
+        dict_with_letters = self.count_values(string_with_letters)
         with open('letters.csv', 'w', newline='') as letters_file:  # open file with posts
             headers = ['letter in lower case', 'number of letters in any case', 'number of letter in uppercase',
                        '% of letter in all letters']
@@ -90,6 +95,6 @@ class CountLetters:
                      '% of letter in all letters': f'{dict_with_letters[key][2]:.2f}%'})
 
 
-# define_format() # to add new posts in posts.txt file
-CountWords().write_words_csv()
-CountLetters().write_letters_csv()
+if __name__ == '__main__':
+    CountWords().write_words_csv()
+    CountLetters().write_letters_csv()
